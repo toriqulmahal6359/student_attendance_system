6eb58a7dabb6db733a678a9fcdd9ddcf90d8074b
toriqulmahal6359 <toriqulmahal6359@yahoo.com>
2023-05-04 11:58:17 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/footer.css">
+    </head>
+    <body>
+        <footer>
+            <?php  
+               $current_year = date("Y");
+               echo "<h3>&copy; ".$current_year." By Md. Toriqul Mahal. All Rights Reserved.</h3>";  
+            ?>
+        </footer>
+    </body>
+</html>
